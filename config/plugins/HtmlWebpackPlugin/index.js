dac90c67b40993776417043b154c05c3903661f6
imgrau <sulina01@mail.ru>
2023-05-01 23:42:03 +0300

    run prettier

--- config/plugins/HtmlWebpackPlugin/index.js
+++ config/plugins/HtmlWebpackPlugin/index.js
@@ -1,8 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-console.log(path.resolve(__dirname, '../../../src/index.html'));
-
 module.exports = new HtmlWebpackPlugin({
     template: path.resolve(__dirname, '../../../src/index.html'),
     filename: 'index.html',
